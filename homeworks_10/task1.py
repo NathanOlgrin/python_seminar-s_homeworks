e25d66d6fd3132941d4594bce21456109a8bf028
NathanOlgrin <matrica_1997@mail.ru>
2023-10-20 18:01:31 +0300

    task1.py

--- homeworks_10/task1.py
+++ homeworks_10/task1.py
@@ -7,3 +7,10 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+data['robot'] = data['whoAmI'].apply(lambda x: 1 if x == 'robot' else 0)
+data['human'] = data['whoAmI'].apply(lambda x: 1 if x == 'human' else 0)
+
+data = data.drop('whoAmI', axis = 1)
+
+print(data.head())
